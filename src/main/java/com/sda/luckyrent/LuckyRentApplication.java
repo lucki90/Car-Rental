56e162f9c6402ba4b5cad25be199ac9bfc071d4e
Tomasz Lucki <lucki@email.com>
2018-08-07 09:51:33 +0200

    first commit

---
+++ src/main/java/com/sda/luckyrent/LuckyRentApplication.java
@@ -0,0 +1,12 @@
+package com.sda.luckyrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckyRentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckyRentApplication.class, args);
+    }
+}
